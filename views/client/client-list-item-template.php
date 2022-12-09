d0ddda876c26c5bd6d914cb255645f6dc0ab23e8
Hans Mbanugu[PC-home] <HansMbangu@gmail.com>
2022-12-09 07:40:42 +0200

    template

--- views/client/client-list-item-template.php
+++ views/client/client-list-item-template.php
@@ -1,8 +1,16 @@
 <?php
-
+  $name = $model['name'];
+  //id="list-home-list"
+  //var_dump($name);
 ?>
 
-    <a class="list-group-item list-group-item-action" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">
+    <a  class="list-group-item list-group-item-action"  
+        id=<?php echo "list-{$name}-list" ?? null ?> 
+        data-toggle="list" 
+        href=<?php echo "#list-{$name}" ?> 
+        role="tab" 
+        aria-controls=<?php echo $name ?? null ?>
+    >
         <ul class="list-unstyled">
           <li>Name: <?php  echo $model['name'] ?? null ?></li>
           <li>Code: <?php  echo $model['code'] ?? null ?></li>
--- views/client/client-list-template.php
+++ views/client/client-list-template.php
@@ -2,12 +2,33 @@
   use suluuboi\phpmvc\Controller;
   $controller = new Controller();
   $obj=[
-    'name'=>'Hans'
+    'name'=>'Hans',
+    'code'=>'200FVN'
   ];
+  //echo '<pre>';
+  //var_dump($obj);
+  //echo '</pre>';
 ?>
+</br>
+<?php
+  //echo '<pre>';
+  //var_dump($model);
+  //echo '</pre>';
+?>
+
 <div class="row">
   <div class="col-4">
     <div class="list-group" id="list-tab" role="tablist">
+      <?php 
+        foreach($model as $key => $client ){
+          //echo "{$key} ==> {$client['name']}";
+          echo $client['name'];
+          $a = $controller->renderTemplate('client/client-list-item-template',[ 'model' => $obj ]);
+          echo $a;
+          echo '</br>';
+          //var_dump($client);
+        } 
+      ?>
       <?php echo $controller->renderTemplate('client/client-list-item-template',[ 'model' => $obj ]) ?>
       <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
       <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
@@ -16,9 +37,10 @@
   </div>
   <div class="col-8">
     <div class="tab-content" id="nav-tabContent">
-      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+      <!--<div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
         Home Data
-      </div>
+      </div>-->
+      <?php echo $controller->renderTemplate('client/client-list-item-content-template',[ 'model' => [] ]) ?>
       <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">Profile Data</div>
       <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">Message Data</div>
       <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">Settings Data</div>
